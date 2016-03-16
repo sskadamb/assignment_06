867631f9750118a94e5e68546f24919a06b92023
Sagarika Kadambi <sskadamb@asu.edu>
2016-03-15 20:54:56 -0700

    fixing imports 2

--- point.py
+++ point.py
@@ -2,8 +2,7 @@ import unittest
 import sys
 import os
 #sys.path.insert(0, os.path.abspath('..'))
-from utils import check_coincident,shift_point
-#from utils import check_coincident, shift_point
+
 
 class Point(object):
     def __init__(self,x,y,mark={}):
